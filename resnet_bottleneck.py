6ec2e40e7286ff44fee4e08efadb0e22c8d48a46
Aaron Bengochea <71156554+aaronbengochea@users.noreply.github.com>
2025-03-09 10:06:29 -0400

    cleanup

--- resnet_bottleneck.py
+++ resnet_bottleneck.py
@@ -19,18 +19,19 @@ class BasicBlock(nn.Module):
         self.shortcut = nn.Sequential()
 
         if stride != 1 or in_channels != out_channels:
-
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_channels, out_channels, kernel_size=skip_kernel_size, stride=stride, padding=skip_kernel_size//2, bias=False),
                 nn.BatchNorm2d(out_channels)
             )
+            
+        self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
         identity = x
         x = self.conv1(x)
         x = self.conv2(x)
         x += self.shortcut(identity)
-        x = nn.ReLU(inplace=True)(x)
+        x = self.relu(x)
         return x
     
 
@@ -39,30 +40,30 @@ class BottleneckBlock(nn.Module):
         super(BottleneckBlock, self).__init__()
         
         self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=skip_kernel_size, bias=False),
+            nn.Conv2d(in_channels, out_channels, kernel_size=skip_kernel_size, padding=skip_kernel_size//2, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True)
-            
         )
 
         self.conv2 = nn.Sequential(
             nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=kernel_size//2, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True)
-            
         )
 
         self.conv3 = nn.Sequential(
-            nn.Conv2d(out_channels, out_channels * expansion_factor, kernel_size=skip_kernel_size, bias=False),
+            nn.Conv2d(out_channels, out_channels * expansion_factor, kernel_size=skip_kernel_size, padding=skip_kernel_size//2, bias=False),
             nn.BatchNorm2d(out_channels * expansion_factor)
         )
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_channels != out_channels * expansion_factor: 
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels * expansion_factor, kernel_size=skip_kernel_size, stride=stride, bias=False),
+                nn.Conv2d(in_channels, out_channels * expansion_factor, kernel_size=skip_kernel_size, stride=stride, padding=skip_kernel_size//2, bias=False),
                 nn.BatchNorm2d(out_channels * expansion_factor)
             )
+            
+        self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
         identity = x
@@ -70,10 +71,8 @@ class BottleneckBlock(nn.Module):
         x = self.conv2(x)
         x = self.conv3(x)
         x += self.shortcut(identity)
-        x = nn.ReLU(inplace=True)(x)
+        x = self.relu(x)
         return x
-
-
 
 
 class ResNetBottleneck(nn.Module):
@@ -81,22 +80,26 @@ class ResNetBottleneck(nn.Module):
         super(ResNetBottleneck, self).__init__()
         self.in_channels = channels_per_layer[0]
         self.name = name
-
+        self.expansion_factor = expansion_factor
 
         self.input_layer = nn.Sequential(
             nn.Conv2d(starting_input_channels, self.in_channels, kernel_size=kernels_per_layer[0], stride=1, padding=kernels_per_layer[0]//2, bias=False),
             nn.BatchNorm2d(self.in_channels),
             nn.ReLU(inplace=True)
-            
         )
 
         self.residual_layers = self._make_layers(blocks_per_layer, channels_per_layer, kernels_per_layer, skip_kernels_per_layer, expansion_factor)
 
-
+        # Calculate the final output channel size
+        final_channels = channels_per_layer[-1] * expansion_factor
+        
+        # Calculate the feature map size after pooling
+        feature_size = pool_size * pool_size
+        
         self.output_layer = nn.Sequential(
             nn.AdaptiveAvgPool2d(pool_size),
             nn.Flatten(),
-            nn.Linear(channels_per_layer[-1] * expansion_factor, num_classes)
+            nn.Linear(final_channels * feature_size, num_classes)
         )
 
 
@@ -104,17 +107,17 @@ class ResNetBottleneck(nn.Module):
         blocks = []
         strides = []
         for i in range(num_blocks):
-            if i != 0:
-                stride = 1
-
-            blocks.append(BottleneckBlock(self.in_channels, out_channels, kernel_size, skip_kernel_size, stride, expansion_factor))
-            strides.append(stride)
+            current_stride = stride if i == 0 else 1
+            if i == 0:
+                strides.append(current_stride)
+                
+            blocks.append(BottleneckBlock(self.in_channels, out_channels, kernel_size, skip_kernel_size, current_stride, expansion_factor))
             self.in_channels = out_channels * expansion_factor
         
-        print(strides, '\n')
+        if strides:
+            print(strides, '\n')
 
         return nn.Sequential(*blocks)
-
 
 
     def _make_layers(self, blocks_per_layer, channels_per_layer, kernels_per_layer, skip_kernels_per_layer, expansion_factor):
@@ -134,7 +137,6 @@ class ResNetBottleneck(nn.Module):
         return nn.Sequential(*layers)
 
 
-
     def forward(self, x):
         x = self.input_layer(x)
         x = self.residual_layers(x)
@@ -142,15 +144,13 @@ class ResNetBottleneck(nn.Module):
         return x
 
 
-
-def create_bottleneck_model(blocks_per_layer, channels_per_layer, kernels_per_layer, skip_kernels_per_layer, pool_size, starting_input_channels, expansion_factor, name):
-    return ResNetBottleneck(blocks_per_layer, channels_per_layer, kernels_per_layer, skip_kernels_per_layer, pool_size, starting_input_channels, expansion_factor, name)
+def create_bottleneck_model(blocks_per_layer, channels_per_layer, kernels_per_layer, skip_kernels_per_layer, pool_size, starting_input_channels, expansion_factor, name, num_classes=10):
+    return ResNetBottleneck(blocks_per_layer, channels_per_layer, kernels_per_layer, skip_kernels_per_layer, pool_size, starting_input_channels, expansion_factor, name, num_classes)
 
 
 if __name__ == "__main__":
     model = create_bottleneck_model(
         blocks_per_layer = [1, 1, 3, 1],
-        #channels_per_layer = [64, 128, 256, 512],
         channels_per_layer = [49, 98, 196, 392],
         kernels_per_layer = [3, 3, 3, 3],
         skip_kernels_per_layer = [1, 1, 1, 1],
@@ -162,4 +162,3 @@ if __name__ == "__main__":
     
     print('Total model parameters:', sum(p.numel() for p in model.parameters() if p.requires_grad))
     summary(model, (3, 32, 32))
-
